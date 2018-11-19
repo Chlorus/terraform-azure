e40c61710cd072feef997dc6e6367d2733cbe0ee
Chlorus <alles@chlor.us>
2018-11-19 15:01:29 -0700

    Basic provider setup stuff

---
+++ provider.tf
@@ -0,0 +1,16 @@
+#Configuration for Azure
+provider "azurerm" {
+    environment = "public"
+}  
+
+#Remote state
+
+terraform {
+    backend "azurerm" {
+        storage_account_name = "chlorusterraform"
+        container_name       = "tfstate"
+        key                  = "prod.terraform.tfstate"
+    }
+}
+
+
